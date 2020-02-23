040eb6bcd2afb5b07f06d4055ca8c3f68e04eb42
Cedric St-Jean <cedric.stjean@gmail.com>
2020-02-22 23:11:49 -0500

    Basic conference calling works

--- src/ConferenceCall.jl
+++ src/ConferenceCall.jl
@@ -2,25 +2,36 @@ module ConferenceCall
 
 using MacroTools: splitdef, combinedef, @capture
 
+export @confcalled
+
+val_value(::Type{Val{T}}) where T = T
+
 available_vals(fn) = (m.sig.types[2] for m in methods(fn).ms)
+sorted_available_vals(fn) = sort(collect(available_vals(fn)), by=val_value)
 
 @generated function call_all_methods_tuple(fn, args...)
     quote
         tuple($([:(fn($v(), args...)) 
-                 for v in available_vals(fn.instance)]...))
+                 for v in sorted_available_vals(fn.instance)]...))
+    end
+end
+
+impl_name(fname::Symbol) = Symbol(fname, "_impl")
+
+macro confcalled(fn_def)
+    if @capture(fn_def, function fname_ end)
+        esc(:($fname(args...) =
+              $ConferenceCall.call_all_methods_tuple($(impl_name(fname)), args...)))
+    else
+        todo()
     end
 end
 
 macro confcalled(key, fn_def)
-    impl_name(fname::Symbol) = 
-    if @capture(fn_def, function fname_ end)
-        esc(:($fname(args...) =
-              $ConferenceCall.call_all_methods_tuple($(impl_name(fname)), args...)))
-    elseif 
-        di = copy(splitdef(fn_def))
-        di[:args] = tuple(:(::Val{$key}), $(di[:args]...))
-        di[:name] = impl_name(di[:name])
-    end
+    di = copy(splitdef(fn_def))
+    di[:args] = tuple(:(::Val{$key}), di[:args]...)
+    di[:name] = impl_name(di[:name])
+    return esc(combinedef(di))
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
-using ConferenceCalls
+using ConferenceCall
 using Test
 
-@testset "ConferenceCalls.jl" begin
+@testset "ConferenceCall.jl" begin
     # Write your own tests here.
+    @confcalled function foo end
+
+    @confcalled 1 foo() = :a
+    @confcalled 2 foo() = :b
+
+    @test foo() == (:a, :b)
 end
